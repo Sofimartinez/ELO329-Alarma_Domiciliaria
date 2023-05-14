d7c8927b4c3e964faf7480e7488c0ee56c9f90f1
Darael Badilla <127801622+Darional@users.noreply.github.com>
2023-05-14 18:22:01 -0400

    Creación de JavaDoc en PIR_Detector.java
    Se crea el JavaDoc solicitado para cada método de la clase.

--- PIR_Detector.java
+++ PIR_Detector.java
@@ -1,21 +1,43 @@
 import java.util.ArrayList;
+/**
+* Clase que crea un PIR y simula su comportamiento.
+* @author Sofía Martínez, Manuel Torres, Florencia Norambuena, Darael Badilla
+*/
 
 public class PIR_Detector extends Sensor{
+    /**
+    * Constructor del PIR, inicializa un nuevo PIR.
+    * @param view   Objeto visual del PIR.  
+    * @param zone   zona a la que pertenece el objeto.
+    */
     public PIR_Detector(PIR_DetectorView view, int zone){
         super(zone);
         pView = view;
         pView.setPIRModel(this);
     }
+    /**
+    * Al detectarse un movimiento, se actualiza el estado del switch a OPEN.
+    */
     public void detectedMovementSwitch() {
         setState(SwitchState.OPEN);
     }
+        /**
+    * Si no se detecta movimiento, se actualiza el estado del switch a CLOSE.
+    */
     public void notMovementSwitch() {
         setState(SwitchState.CLOSE);
     }
+    /**
+    * Recupera el objeto creado en PIR_DetectorView y que es enlazado al objeto creado en PIR_Detector.
+    * @return el <tt>pView</tt> que fue enlazado con el PIR.
+    */
     public PIR_DetectorView getView(){
         return pView;
     }
-
+    /**
+    * Busca si es que hay una persona dentro de la lista que está ubicada dentro del rango del PIR. Si es así, se activa el método detectedMovementSwitch(). Caso contrario se activa notMovementSwitch()
+    * @param persons lista de la clase personas
+    */
     public void checkPerson(ArrayList<Person> persons){
         Double x_person, y_person;
         Double range_person, angle_person;
@@ -56,6 +78,10 @@ public class PIR_Detector extends Sensor{
             notMovementSwitch();
         }
     }
+    // Campos Privados.
+    /**
+    *   Objeto de características visuales que se relaciona con el PIR.
+    */
     private final PIR_DetectorView pView;
 
 }
