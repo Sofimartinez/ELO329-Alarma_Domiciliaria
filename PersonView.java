7487a49440bb44a345771886069d6ffd81bae2ae
ManuManitoo77 <manuel.torresm@sansano.usm.cl>
2023-05-14 05:43:47 -0400

    ManuManito Update :3

--- PersonView.java
+++ PersonView.java
@@ -22,28 +22,57 @@ public class PersonView extends Group {
         head.setStroke(Color.BLACK);
         person = new Group(body, armLeft, armRight, head);
 
+
         //Person Event
         makeDraggable(person);
         getChildren().addAll(person);
     }
 
     private void makeDraggable(Group node){
-        Delta delta = new Delta();
+        //Delta delta = new Delta();
+        Mouse mouse = new Mouse();
 
         node.setOnMousePressed((e ->{
             //Se obtiene la posición del mouse con respecto a la imagen
-            delta.x = e.getSceneX() - node.getLayoutX();
-            delta.y = e.getSceneY() - node.getLayoutY();
+            //delta.x = e.getSceneX() - node.getLayoutX();
+            //delta.y = e.getSceneY() - node.getLayoutY();
+            mouse.x = e.getSceneX();
+            mouse.y = e.getSceneY();
+
+            //System.out.println(delta.x);
         }));
 
         node.setOnMouseDragged(e -> {
-            Double sceneX = e.getSceneX();
+            /*Double sceneX = e.getSceneX();
             Double sceneY = e.getSceneY();
+
+            double centerX = node.getBoundsInParent().getWidth() / 2;
+            double centerY = node.getBoundsInParent().getHeight() / 2;
+            //System.out.println("Centro del grupo: (" + centerX + ", " + centerY + ")");
+
             //Se mueve la vista persona y además se modifica las propiedades de Person para conectarlo con el PIR
             node.setLayoutX(sceneX - delta.x);
             node.setLayoutY(sceneY - delta.y);
-            personModel.horizontalMove(sceneX-10);
-            personModel.verticalMove(sceneY-10);
+
+            personModel.horizontalMove(sceneX-centerX);
+            personModel.verticalMove(sceneY-centerY);
+            //System.out.println((sceneX-centerX) + ", " + (sceneY-centerY));
+            */
+            double deltaX = e.getSceneX() - mouse.x;
+            double deltaY = e.getSceneY() - mouse.y;
+
+            node.setLayoutX(node.getLayoutX() + deltaX);
+            node.setLayoutY(node.getLayoutY() + deltaY);
+
+            double centerX = node.getBoundsInParent().getMinX() + node.getBoundsInParent().getWidth()/2;
+            double centerY = node.getBoundsInParent().getMinY() + node.getBoundsInParent().getHeight()/2;
+            personModel.horizontalMove(centerX);
+            personModel.verticalMove(centerY);
+
+            mouse.x = e.getSceneX();
+            mouse.y = e.getSceneY();
+
+            System.out.println("Centro del grupo: (" + centerX + ", " + centerY + ")");
         });
 
 
@@ -52,6 +81,7 @@ public class PersonView extends Group {
         personModel = model;
     }
     protected class Delta{double x,y;}
+    protected class Mouse{double x,y;}
     private Person personModel;
     private Group person;
 }
