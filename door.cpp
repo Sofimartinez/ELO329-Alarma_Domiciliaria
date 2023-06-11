aa63d1425ac21d06b484b84f283eedb3cd09b99b
Sofia Martinez <sofiamartineza45@gmail.com>
2023-06-11 12:49:14 -0400

    Stage 1

---
+++ door.cpp
@@ -0,0 +1,14 @@
+#include "door.h"
+
+Door::Door()
+{
+
+}
+Door::Door(MagneticSensor sensor): magneticSensor(sensor) {}
+void Door::changeState() {
+        if (magneticSensor.isClose())
+            magneticSensor.setSensorOpen();
+        else
+            magneticSensor.setSensorClose();
+}
+
---
+++ sensor.cpp
@@ -0,0 +1,17 @@
+#include "sensor.h"
+#include <iostream>
+using namespace std;
+Sensor::Sensor(int z, bool close){
+    this->zone = z;
+    this->close = close;
+}
+bool Sensor::isClose() const {
+    return close;
+}
+int Sensor::getZone() const {
+    return zone;
+}
+void Sensor::setClose(bool isClose) {
+    this->close = isClose;
+    cout << "Sensor is "<< (close?"close.":"open.") << endl;
+}
